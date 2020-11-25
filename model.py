bf01a4ff0c63ce9b0d7b959163e91ed5e35587ca
Aw Yew Lim <yewlim00@gmail.com>
2020-11-25 14:43:04 +0800

    Update model.py

--- model.py
+++ model.py
@@ -137,7 +137,7 @@ def model(data, ix_to_char, char_to_ix, num_iterations = 35000, n_a = 50, names 
     parameters = initialize_parameters(n_a, n_x, n_y)
     loss = get_initial_loss(vocab_size, names)
     
-    with open("gaming_name.txt") as f:
+    with open("training.txt") as f:
         examples = f.readlines()
     examples = [x.lower().strip() for x in examples]
     np.random.shuffle(examples)
@@ -163,77 +163,3 @@ def model(data, ix_to_char, char_to_ix, num_iterations = 35000, n_a = 50, names 
                 print_sample(sampled_indices, ix_to_char)
         
     return parameters
-
-'''
-# GRADED FUNCTION: model
-
-def model(data, ix_to_char, char_to_ix, num_iterations = 35000, n_a = 50, names = 7, vocab_size = 27, verbose = False):
-
-    # Retrieve n_x and n_y from vocab_size
-    n_x, n_y = vocab_size, vocab_size
-    
-    # Initialize parameters
-    parameters = initialize_parameters(n_a, n_x, n_y)
-    
-    # Initialize loss (this is required because we want to smooth our loss)
-    loss = get_initial_loss(vocab_size, names)
-    
-    # Build list of all dinosaur names (training examples).
-    with open("gaming_names.txt") as f:
-        examples = f.readlines()
-    examples = [x.lower().strip() for x in examples]
-    
-    # Shuffle list of all dinosaur names
-    np.random.shuffle(examples)
-    
-    # Initialize the hidden state of your LSTM
-    a_prev = np.zeros((n_a, 1))
-    
-    # Optimization loop
-    for j in range(num_iterations):
-        
-        ### START CODE HERE ###
-        
-        # Set the index `idx` (see instructions above)
-        idx = j % len(examples)
-        
-        # Set the input X (see instructions above)
-        single_example = examples[idx]
-        single_example_chars = [c for c in single_example]
-        single_example_ix = [char_to_ix[ch] for ch in single_example_chars]
-        X = [None] + single_example_ix
-        
-        # Set the labels Y (see instructions above)
-        ix_newline = [char_to_ix['\n']]
-        Y = X[1:] + ix_newline
-
-        # Perform one optimization step: Forward-prop -> Backward-prop -> Clip -> Update parameters
-        # Choose a learning rate of 0.01
-        curr_loss, gradients, a_prev = optimize(X, Y, a_prev, parameters, vocab_size, learning_rate = 0.01)
-        
-        ### END CODE HERE ###
-        
-        # debug statements to aid in correctly forming X, Y
-
-        
-        # Use a latency trick to keep the loss smooth. It happens here to accelerate the training.
-        loss = smooth(loss, curr_loss)
-
-        # Every 2000 Iteration, generate "n" characters thanks to sample() to check if the model is learning properly
-        if j % 2000 == 0:
-            
-            print('Iteration: %d, Loss: %f' % (j, loss) + '\n')
-            
-            # The number of dinosaur names to print
-            for name in range(names):
-                
-                # Sample indices and print them
-                sampled_indices = sample(parameters, char_to_ix)
-                print_sample(sampled_indices, ix_to_char)
-                
- # To get the same result (for grading purposes), increment the seed by one. 
-      
-            print('\n')
-        
-    return parameters
-'''
