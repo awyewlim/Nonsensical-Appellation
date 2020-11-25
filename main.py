3c4ecbba8c4a64e314772f32118867b6dc3fb291
Aw Yew Lim <yewlim00@gmail.com>
2020-11-25 14:39:29 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import numpy as np
+import random
+from model import *
+
+data = open('gaming_name.txt', 'r').read()
+data= data.lower()
+chars = list(set(data))
+data_size, vocab_size = len(data), len(chars)
+#print('There are %d total characters and %d unique characters in your data.' % (data_size, vocab_size))
+
+chars = sorted(chars)
+#print(chars)
+
+char_to_ix = { ch:i for i,ch in enumerate(chars) }
+ix_to_char = { i:ch for i,ch in enumerate(chars) }
+
+parameters = model(data, ix_to_char, char_to_ix, num_iterations = 100000, names = 0, vocab_size = vocab_size, verbose = True)
+
+print('Welcome to Gaming Name Generator!' + '\n')
+more = None
+while more != 'N':
+  num = int(input('Numbers of names you are looking for: '))
+  print('\n')
+  for i in range(num):
+    sampled_indices = sample(parameters, char_to_ix)
+    print_sample(sampled_indices, ix_to_char)
+  print('\n')
+  more = input('Looking more? (Enter "N" to exit): ')
+print('\n')
+print('Thanks for using Gaming Name Generator!')
+
---
+++ model.py
@@ -0,0 +1,239 @@
+import numpy as np
+import random
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
+
+def smooth(loss, cur_loss):
+    return loss * 0.999 + cur_loss * 0.001
+
+def print_sample(sample_ix, ix_to_char):
+    txt = ''.join(ix_to_char[ix] for ix in sample_ix)
+    txt = txt[0].upper() + txt[1:]  # capitalize first character 
+    print ('%s' % (txt, ), end='')
+
+def get_initial_loss(vocab_size, seq_length):
+    return -np.log(1.0/vocab_size)*seq_length
+
+def initialize_parameters(n_a, n_x, n_y):
+    Wax = np.random.randn(n_a, n_x)*0.01 # input to hidden
+    Waa = np.random.randn(n_a, n_a)*0.01 # hidden to hidden
+    Wya = np.random.randn(n_y, n_a)*0.01 # hidden to output
+    b = np.zeros((n_a, 1)) # hidden bias
+    by = np.zeros((n_y, 1)) # output bias
+    
+    parameters = {"Wax": Wax, "Waa": Waa, "Wya": Wya, "b": b,"by": by}
+    
+    return parameters
+
+def rnn_step_forward(parameters, a_prev, x):
+    Waa, Wax, Wya, by, b = parameters['Waa'], parameters['Wax'], parameters['Wya'], parameters['by'], parameters['b']
+    a_next = np.tanh(np.dot(Wax, x) + np.dot(Waa, a_prev) + b) # hidden state
+    p_t = softmax(np.dot(Wya, a_next) + by) # unnormalized log probabilities for next chars # probabilities for next chars 
+    
+    return a_next, p_t
+
+def rnn_step_backward(dy, gradients, parameters, x, a, a_prev):
+    gradients['dWya'] += np.dot(dy, a.T)
+    gradients['dby'] += dy
+    da = np.dot(parameters['Wya'].T, dy) + gradients['da_next'] # backprop into h
+    daraw = (1 - a * a) * da # backprop through tanh nonlinearity
+    gradients['db'] += daraw
+    gradients['dWax'] += np.dot(daraw, x.T)
+    gradients['dWaa'] += np.dot(daraw, a_prev.T)
+    gradients['da_next'] = np.dot(parameters['Waa'].T, daraw)
+    return gradients
+
+def update_parameters(parameters, gradients, lr):
+    parameters['Wax'] += -lr * gradients['dWax']
+    parameters['Waa'] += -lr * gradients['dWaa']
+    parameters['Wya'] += -lr * gradients['dWya']
+    parameters['b']  += -lr * gradients['db']
+    parameters['by']  += -lr * gradients['dby']
+    return parameters
+
+def rnn_forward(X, Y, a0, parameters, vocab_size):
+    x, a, y_hat = {}, {}, {}
+    a[-1] = np.copy(a0)
+    loss = 0
+    
+    for t in range(len(X)):
+        x[t] = np.zeros((vocab_size,1)) 
+        if (X[t] != None):
+            x[t][X[t]] = 1
+        
+        a[t], y_hat[t] = rnn_step_forward(parameters, a[t-1], x[t])
+        loss -= np.log(y_hat[t][Y[t],0])
+        
+    cache = (y_hat, a, x)
+        
+    return loss, cache
+
+def rnn_backward(X, Y, parameters, cache):
+    gradients = {}
+    (y_hat, a, x) = cache
+    Waa, Wax, Wya, by, b = parameters['Waa'], parameters['Wax'], parameters['Wya'], parameters['by'], parameters['b']
+    gradients['dWax'], gradients['dWaa'], gradients['dWya'] = np.zeros_like(Wax), np.zeros_like(Waa), np.zeros_like(Wya)
+    gradients['db'], gradients['dby'] = np.zeros_like(b), np.zeros_like(by)
+    gradients['da_next'] = np.zeros_like(a[0])
+    
+    for t in reversed(range(len(X))):
+        dy = np.copy(y_hat[t])
+        dy[Y[t]] -= 1
+        gradients = rnn_step_backward(dy, gradients, parameters, x[t], a[t], a[t-1])
+    
+    return gradients, a
+
+def clip(gradients, maxValue):
+    dWaa, dWax, dWya, db, dby = gradients['dWaa'], gradients['dWax'], gradients['dWya'], gradients['db'], gradients['dby']
+    for gradient in [dWax, dWaa, dWya, db, dby]:
+        np.clip(gradient, -1*maxValue, maxValue, out=gradient)
+    gradients = {"dWaa": dWaa, "dWax": dWax, "dWya": dWya, "db": db, "dby": dby}
+        
+    return gradients
+
+def sample(parameters, char_to_ix):
+    Waa, Wax, Wya, by, b = parameters['Waa'], parameters['Wax'], parameters['Wya'], parameters['by'], parameters['b']
+    vocab_size = by.shape[0]
+    n_a = Waa.shape[1]
+
+    x = np.zeros((vocab_size, 1))
+    a_prev = np.zeros((n_a, 1))
+
+    indices = []
+    idx = -1
+    counter = 0
+    newline_character = char_to_ix['\n']
+
+    while(idx != newline_character and counter != 50):
+        a = np.tanh(np.dot(Wax, x) + np.dot(Waa, a_prev) + b)
+        z = np.dot(Wya, a) + by
+        y = softmax(z)
+
+        idx = np.random.choice([i for i in range(vocab_size)], p = y.ravel())
+        indices.append(idx)
+        x = np.zeros((vocab_size, 1))
+        x[idx] = 1
+        a_prev = a
+        counter += 1
+
+    if(counter == 50):
+        indices.append(char_to_ix['\n'])
+
+    return indices
+
+def optimize(X, Y, a_prev, parameters, vocab_size, learning_rate = 0.01):
+    loss, cache = rnn_forward(X, Y, a_prev, parameters, vocab_size)
+    gradients, a = rnn_backward(X, Y, parameters, cache)
+    gradients = clip(gradients, 5)
+    parameters = update_parameters(parameters, gradients, learning_rate)
+
+    return loss, gradients, a[len(X)-1]
+
+def model(data, ix_to_char, char_to_ix, num_iterations = 35000, n_a = 50, names = 7, vocab_size = 27, verbose = False):
+    n_x, n_y = vocab_size, vocab_size
+    
+    parameters = initialize_parameters(n_a, n_x, n_y)
+    loss = get_initial_loss(vocab_size, names)
+    
+    with open("gaming_name.txt") as f:
+        examples = f.readlines()
+    examples = [x.lower().strip() for x in examples]
+    np.random.shuffle(examples)
+    
+    a_prev = np.zeros((n_a, 1))
+    print('Your gaming name maker is learning. Please wait patiently...')
+    for j in range(num_iterations):
+        idx = j % len(examples)
+        single_example = examples[idx]
+        single_example_chars = [c for c in single_example]
+        single_example_ix = [char_to_ix[ch] for ch in single_example_chars]
+        X = [None] + single_example_ix
+        ix_newline = [char_to_ix['\n']]
+        Y = X[1:] + ix_newline
+        curr_loss, gradients, a_prev = optimize(X, Y, a_prev, parameters, vocab_size, learning_rate = 0.01)
+
+        loss = smooth(loss, curr_loss)
+        progress = (j/num_iterations * 100)
+        if progress % 10 == 0:
+            print('Completed: %d/100, Loss: %f' % (progress, loss) + '\n')
+            for name in range(names):
+                sampled_indices = sample(parameters, char_to_ix)
+                print_sample(sampled_indices, ix_to_char)
+        
+    return parameters
+
+'''
+# GRADED FUNCTION: model
+
+def model(data, ix_to_char, char_to_ix, num_iterations = 35000, n_a = 50, names = 7, vocab_size = 27, verbose = False):
+
+    # Retrieve n_x and n_y from vocab_size
+    n_x, n_y = vocab_size, vocab_size
+    
+    # Initialize parameters
+    parameters = initialize_parameters(n_a, n_x, n_y)
+    
+    # Initialize loss (this is required because we want to smooth our loss)
+    loss = get_initial_loss(vocab_size, names)
+    
+    # Build list of all dinosaur names (training examples).
+    with open("gaming_names.txt") as f:
+        examples = f.readlines()
+    examples = [x.lower().strip() for x in examples]
+    
+    # Shuffle list of all dinosaur names
+    np.random.shuffle(examples)
+    
+    # Initialize the hidden state of your LSTM
+    a_prev = np.zeros((n_a, 1))
+    
+    # Optimization loop
+    for j in range(num_iterations):
+        
+        ### START CODE HERE ###
+        
+        # Set the index `idx` (see instructions above)
+        idx = j % len(examples)
+        
+        # Set the input X (see instructions above)
+        single_example = examples[idx]
+        single_example_chars = [c for c in single_example]
+        single_example_ix = [char_to_ix[ch] for ch in single_example_chars]
+        X = [None] + single_example_ix
+        
+        # Set the labels Y (see instructions above)
+        ix_newline = [char_to_ix['\n']]
+        Y = X[1:] + ix_newline
+
+        # Perform one optimization step: Forward-prop -> Backward-prop -> Clip -> Update parameters
+        # Choose a learning rate of 0.01
+        curr_loss, gradients, a_prev = optimize(X, Y, a_prev, parameters, vocab_size, learning_rate = 0.01)
+        
+        ### END CODE HERE ###
+        
+        # debug statements to aid in correctly forming X, Y
+
+        
+        # Use a latency trick to keep the loss smooth. It happens here to accelerate the training.
+        loss = smooth(loss, curr_loss)
+
+        # Every 2000 Iteration, generate "n" characters thanks to sample() to check if the model is learning properly
+        if j % 2000 == 0:
+            
+            print('Iteration: %d, Loss: %f' % (j, loss) + '\n')
+            
+            # The number of dinosaur names to print
+            for name in range(names):
+                
+                # Sample indices and print them
+                sampled_indices = sample(parameters, char_to_ix)
+                print_sample(sampled_indices, ix_to_char)
+                
+ # To get the same result (for grading purposes), increment the seed by one. 
+      
+            print('\n')
+        
+    return parameters
+'''
